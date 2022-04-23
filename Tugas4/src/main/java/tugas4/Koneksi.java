761241128c6145edcd27b026163c2064b9df3538
Eatriceeveryday <82435987+Eatriceeveryday@users.noreply.github.com>
2022-04-23 14:34:48 +0700

    Add files via upload

---
+++ Tugas4/src/main/java/tugas4/Koneksi.java
@@ -0,0 +1,24 @@
+package tugas4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Koneksi {
+    String dbUrl = "jdbc:mysql://localhost/praktikum";
+    String dbUsername = "root";
+    String dbPassword = "";
+    static final String driver = "com.mysql.cj.jdbc.Driver";
+    Connection konek;
+    Statement statement;
+
+    public Koneksi() {
+        try{
+            Class.forName(driver);
+            konek=DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+            System.out.println("Koneksi Berhasil");
+        }catch(Exception ex){
+            System.out.println("Koneksi gagal");
+        }
+    }
+}
